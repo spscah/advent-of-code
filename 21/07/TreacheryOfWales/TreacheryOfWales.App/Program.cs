2bf0b4d9aeac4ed231b38c65bb797f26c077481f
Chris Harrison <cah@stpaulsschool.org.uk>
2021-12-07 09:23:30 +0000

    day 7 - trivial
    Although I feel that there must be an easier way

---
+++ 21/07/TreacheryOfWales/TreacheryOfWales.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode.Lib;
+using System.Linq;
+
+namespace GiantSquid.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TODAY = 7;
+            const bool TEST = false;
+            IList<int> data = TODAY.CsvToIntegers(TEST);
+            Console.WriteLine(data.Distinct().Select(d => TotalFuel(d, data, PartOne)).Min());
+            Console.WriteLine(data.Distinct().Select(d => TotalFuel(d, data, PartTwo)).Min());
+        }
+
+        static int TotalFuel(int alignment, IList<int> data, Func<int,int,int> fn)
+        {
+            return data.Select(i => fn(alignment,i)).Sum();
+        }
+
+        static int PartOne(int alignment, int i)
+        {
+            return Math.Abs(alignment - i);
+        }
+
+        static int PartTwo(int alignment, int i)
+        {
+            int a = Math.Abs(alignment - i);
+            return a * (a + 1) / 2;
+        }
+
+
+    }
+}
